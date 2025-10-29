e8f690d24b6a576d013a033224ad28356d5bc751
dmitryloschinin <loschinin@list.ru>
2025-10-30 00:25:19 +0300

    Timeline.module refactor

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,26 +1,26 @@
 import { useEvents } from '@/hooks/useEvents';
 import { useState } from 'react';
 import CirclePagination from './CirclePagination/CirclePagination';
-import CustomSwiper from './CustomSwiper/CustomSwiper'; // Import the new CustomSwiper component
+import CustomSwiper from './CustomSwiper/CustomSwiper';
 import styles from './Timeline.module.scss';
 import PeriodNavigation from './PeriodNavigation/PeriodNavigation';
 import BulletPagination from './BulletPagination/BulletPagination';
 import YearsContainer from './YearsContainer/YearsContainer';
 
-const LIMIT = 6; // Number of years per period
+const LIMIT = 6; // Количество лет в одном периоде
 
 const Timeline = () => {
   const [page, setPage] = useState(1);
-
   const { data: initialData } = useEvents(1, LIMIT);
   const initialTotalPages = initialData?.totalPages || 0;
 
   return (
-    <div className={styles.timelineContainer}>
-      <div className={styles.timelineHeader}>
+    <div className={styles.container}>
+      <header className={styles.header}>
         <h1 className={styles.title}>Исторические даты</h1>
 
         <YearsContainer page={page} limit={LIMIT} />
+
         <CirclePagination
           totalPages={initialTotalPages}
           currentPage={page}
@@ -29,20 +29,23 @@ const Timeline = () => {
         />
 
         <hr className={styles.divider} />
-      </div>
-      <div className={styles.timelineBody}>
+      </header>
+
+      <main className={styles.main}>
         <PeriodNavigation
           page={page}
           totalPages={initialTotalPages}
           setPage={setPage}
         />
+
         <CustomSwiper page={page} limit={LIMIT} />
+
         <BulletPagination
           totalPages={initialTotalPages}
           currentPage={page}
           onPageChange={setPage}
         />
-      </div>
+      </main>
     </div>
   );
 };
